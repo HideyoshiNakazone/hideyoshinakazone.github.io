7fde907d6fa5b73f078e9b885ee4e34d61b0030a
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2020-10-24 11:08:58 -0300

    24/10/2020

--- Moderno/js/main.js
+++ Moderno/js/main.js
@@ -28,3 +28,28 @@ mnBtn.addEventListener('click', () => {
         }
     });
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('js/sw.js').then(function(
+            registration) {
+                //Registration was successful
+                console.log('ServiceWorker resgistration succesful with scope: '
+                    , registration.scope);
+            }, function(err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    });
+}
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    showInstallPromotion();
+});
--- Moderno/js/sw.js
+++ Moderno/js/sw.js
@@ -1,1 +1,2 @@
 self.addEventListener('fetch', function (event) {});
+console.log('I am a Service Worker!');
