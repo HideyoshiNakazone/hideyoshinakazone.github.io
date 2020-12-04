d6da9bafd6a39881bf195bd3a1db5d5d83e72a77
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2020-12-04 06:06:46 +0000

    PWA Fixed

--- Moderno/js/sw.js
+++ Moderno/js/sw.js
@@ -1,8 +1,2 @@
 self.addEventListener('fetch', function (event) {});
 console.log('I am a Service Worker!');
-
-let deferredPrompt;
-window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault()
-    deferredPrompt = e
-});
